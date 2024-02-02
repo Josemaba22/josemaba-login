a34be5f6f566469471bd7db0fc952c77974075df
jose-barraza_neoris <jose.barraza@neoris.com>
2024-02-01 21:18:35 -0700

    Creando CustomerRestController y AuthenticationService.

---
+++ src/main/java/com/josemaba/security/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.josemaba.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.josemaba.security.dto.RegisteredUser;
+import com.josemaba.security.dto.SaveUser;
+import com.josemaba.security.service.auth.AuthenticationService;
+
+import jakarta.validation.Valid;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    
+    @Autowired
+    private AuthenticationService authenticationService;
+    
+    @PostMapping
+    public ResponseEntity<RegisteredUser> registerOne(@RequestBody @Valid SaveUser newUser){
+        RegisteredUser registeredUser = authenticationService.registerOneCustomer(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }   
+}
---
+++ src/main/java/com/josemaba/security/service/auth/AuthenticationService.java
@@ -0,0 +1,16 @@
+package com.josemaba.security.service.auth;
+
+import org.springframework.stereotype.Service;
+
+import com.josemaba.security.dto.RegisteredUser;
+import com.josemaba.security.dto.SaveUser;
+
+
+@Service
+public class AuthenticationService {
+
+    public RegisteredUser registerOneCustomer(SaveUser newUser) {
+        return null;
+    }
+    
+}
