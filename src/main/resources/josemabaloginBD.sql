8a58f7fcc8cf64aafe5644fdfb6a28530193495e
jose-barraza_neoris <jose.barraza@neoris.com>
2024-03-05 17:04:15 -0700

    Configurando MySQL.

---
+++ src/main/resources/josemabaloginBD.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE josemabalogin;
+#DROP database josemabalogin;
+USE josemabalogin;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    name VARCHAR(255),
+    password VARCHAR(255),
+    role ENUM(
+		'ROLE_ADMISNISTRATOR', 
+		'ROLE_ASSISTANT_ADMINISTRATOR', 
+		'ROLE_CUSTOMER'
+	) -- replace with your actual roles
+);
+
+CREATE TABLE categories (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    status ENUM('ENABLED', 'DISABLED')
+);
+
+CREATE TABLE products (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    price DECIMAL(19, 2),
+    status ENUM('ENABLED', 'DISABLED'),
+    category_id BIGINT
+);
+
+ALTER TABLE products ADD CONSTRAINT FK_products_categories FOREIGN KEY(category_id)
+REFERENCES categories(id);
+
+
+-- CREACIÓN DE CATEGORIAS
+INSERT INTO categories (name, status) VALUES ('Electronica', 'ENABLED');
+INSERT INTO categories (name, status) VALUES ('Ropa', 'ENABLED');
+INSERT INTO categories (name, status) VALUES ('Deportes', 'ENABLED');
+INSERT INTO categories (name, status) VALUES ('Hogar', 'ENABLED');
+
+-- CREACIÓN DE PRODUCTOS
+INSERT INTO products (name, price, status, category_id) VALUES ('Smartphone', 500.00, 'ENABLED', 1);
+INSERT INTO products (name, price, status, category_id) VALUES ('Auriculares Bluetooth', 50.00, 'DISABLED', 1);
+INSERT INTO products (name, price, status, category_id) VALUES ('Tablet', 300.00, 'ENABLED', 1);
+
+INSERT INTO products (name, price, status, category_id) VALUES ('Camiseta', 25.00, 'ENABLED', 2);
+INSERT INTO products (name, price, status, category_id) VALUES ('Pantalones', 35.00, 'ENABLED', 2);
+INSERT INTO products (name, price, status, category_id) VALUES ('Zapatos', 45.00, 'ENABLED', 2);
+
+INSERT INTO products (name, price, status, category_id) VALUES ('Balon de Futbol', 20.00, 'ENABLED', 3);
+INSERT INTO products (name, price, status, category_id) VALUES ('Raqueta de Tenis', 80.00, 'DISABLED', 3);
+
+INSERT INTO products (name, price, status, category_id) VALUES ('Aspiradora', 120.00, 'ENABLED', 4);
+INSERT INTO products (name, price, status, category_id) VALUES ('Licuadora', 50.00, 'ENABLED', 4);
+
+
+SELECT *  FROM users;
