7575ca13256418f942e03a80ae20ac4fc9d439f5
jose-barraza_neoris <jose.barraza@neoris.com>
2024-03-13 23:05:12 -0700

    Creando autorizacion basado en coincidencias de solicitudes HTTP y permisos.

--- src/main/java/com/josemaba/security/config/security/HttpSecurityConfig.java
+++ src/main/java/com/josemaba/security/config/security/HttpSecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.josemaba.security.config.security.filter.JwtAuthenticationFilter;
+import com.josemaba.security.persistence.util.RolePermission;
 
 @Configuration
 @EnableWebSecurity
@@ -30,15 +32,54 @@ public class HttpSecurityConfig {
             .sessionManagement(sessMagConfig -> sessMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
             .authenticationProvider(daoAuthProvider)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .authorizeHttpRequests(authReqConfig -> {
-                authReqConfig.requestMatchers(HttpMethod.POST,"/customers").permitAll();
-                authReqConfig.requestMatchers(HttpMethod.POST,"/auth/authenticate").permitAll();
-                authReqConfig.requestMatchers(HttpMethod.GET,"/auth/validate-token").permitAll();
-
-                authReqConfig.anyRequest().authenticated();
-            })
+            .authorizeHttpRequests(authReqConfig -> buildRequestMatchers(authReqConfig))
             .build();
         return filterChain;
+    }
+
+    private void buildRequestMatchers(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig) {
+
+        // Autorizacion de endpoints de productos
+        authReqConfig.requestMatchers(HttpMethod.GET,"/products")
+            .hasAuthority(RolePermission.READ_ALL_PRODUCTS.name());
+
+        authReqConfig.requestMatchers(HttpMethod.GET,"/products/{productId}")
+            .hasAuthority(RolePermission.READ_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.POST,"/products")
+            .hasAuthority(RolePermission.CREATE_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT,"/products/{productId}")
+            .hasAuthority(RolePermission.UPDATE_ONE_PRODUCT.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT,"/products/{productId}/disabled")
+            .hasAuthority(RolePermission.DISABLE_ONE_PRODUCT.name());
+
+        // Autorizacion de endpoints de categorias
+        authReqConfig.requestMatchers(HttpMethod.GET,"/categories")
+        .hasAuthority(RolePermission.READ_ALL_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.GET,"/categories/{categoryId}")
+            .hasAuthority(RolePermission.READ_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.POST,"/categories")
+            .hasAuthority(RolePermission.CREATE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT,"/categories/{categoryId}")
+            .hasAuthority(RolePermission.UPDATE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT,"/categories/{categoryId}/disabled")
+            .hasAuthority(RolePermission.DISABLE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.PUT,"/auth/profile")
+            .hasAuthority(RolePermission.READ_MY_PROFILE.name());
+            
+        // Autorizacion de endpoints publicos
+        authReqConfig.requestMatchers(HttpMethod.POST,"/customers").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.POST,"/auth/authenticate").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.GET,"/auth/validate-token").permitAll();
+        authReqConfig.anyRequest().authenticated();
+
     }
     
 }
